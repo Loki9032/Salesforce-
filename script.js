84be7e494e81a79c58be03f8366e3fe71eb90c8b
Loki9032 <121725012+Loki9032@users.noreply.github.com>
2023-10-20 13:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+let captchachecked = false;
 function beforeSubmit(){
-    let outputDate = document.querySelector(".outputDate");
-    let inputdate = document.querySelector(".inputDate");
-    console.log('input Date:' , inputdate.value);
-    let formatedDate = new Date(inputdate.value).toLocaleDateString("en-US");
-    outputDate.value = formatedDate;
+        if(captchachecked){     
+        let outputDate = document.querySelector(".outputDate");
+        let inputdate = document.querySelector(".inputDate");
+        let formatedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+        outputDate.value = formatedDate;
+        
+        }else{
+        alert ("Please Check The reCAPTCHA");
+        event.preventDefault();
+        }
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
+
+function capthasuccess(){
+  this.captchachecked = true;
+}
